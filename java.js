6299c2ca89447ef52ff218281c7705157c9c2299
Kaua011RiBeIrO <102737682+Kaua011RiBeIrO@users.noreply.github.com>
2022-05-24 08:23:54 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,119 @@
+var paciente = document.querySelector('#primeiro-paciente')
+console.log(paciente)
+
+var valorHTML = paciente.querySelector('.info-HTML')
+var HTML = parseInt(valorHTML.textContent)
+console.log(HTML)
+
+var valorCSc = paciente.querySelector('.info-CSc')
+var CSc = parseInt(valorCSc.textContent)
+console.log(CSc)
+
+var valorJavaScript = paciente.querySelector('.info-JavaScript')
+var JavaScript = parseInt(valorJavaScript.textContent)
+console.log(JavaScript)
+
+var valorMedia = (HTML + CSc + JavaScript) / 3
+console.log(valorMedia)
+
+var substituirMedia = paciente.querySelector('.info-Media')
+substituirMedia.textContent = valorMedia.toFixed(2)
+
+/////
+
+var paciente = document.querySelector('#segundo-paciente')
+console.log(paciente)
+
+var valorHTML = paciente.querySelector('.info-HTML')
+var HTML = parseInt(valorHTML.textContent)
+console.log(HTML)
+
+var valorCSc = paciente.querySelector('.info-CSc')
+var CSc = parseInt(valorCSc.textContent)
+console.log(CSc)
+
+var valorJavaScript = paciente.querySelector('.info-JavaScript')
+var JavaScript = parseInt(valorJavaScript.textContent)
+console.log(JavaScript)
+
+var valorMedia = (HTML + CSc + JavaScript) / 3
+console.log(valorMedia)
+
+var substituirMedia = paciente.querySelector('.info-Media')
+substituirMedia.textContent = valorMedia.toFixed(2)
+
+/////
+
+var paciente = document.querySelector('#terceiro-paciente')
+console.log(paciente)
+
+var valorHTML = paciente.querySelector('.info-HTML')
+var HTML = parseInt(valorHTML.textContent)
+console.log(HTML)
+
+var valorCSc = paciente.querySelector('.info-CSc')
+var CSc = parseInt(valorCSc.textContent)
+console.log(CSc)
+
+var valorJavaScript = paciente.querySelector('.info-JavaScript')
+var JavaScript = parseInt(valorJavaScript.textContent)
+console.log(JavaScript)
+
+var valorMedia = (HTML + CSc + JavaScript) / 3
+console.log(valorMedia)
+
+var substituirMedia = paciente.querySelector('.info-Media')
+substituirMedia.textContent = valorMedia.toFixed(2)
+
+/////
+
+var paciente = document.querySelector('#quarto-paciente')
+console.log(paciente)
+
+var valorHTML = paciente.querySelector('.info-HTML')
+var HTML = parseInt(valorHTML.textContent)
+console.log(HTML)
+
+var valorCSc = paciente.querySelector('.info-CSc')
+var CSc = parseInt(valorCSc.textContent)
+console.log(CSc)
+
+var valorJavaScript = paciente.querySelector('.info-JavaScript')
+var JavaScript = parseInt(valorJavaScript.textContent)
+console.log(JavaScript)
+
+var valorMedia = (HTML + CSc + JavaScript) / 3
+console.log(valorMedia)
+
+var substituirMedia = paciente.querySelector('.info-Media')
+substituirMedia.textContent = valorMedia.toFixed(2)
+
+/////
+
+var paciente = document.querySelector('#quinto-paciente')
+console.log(paciente)
+
+var valorHTML = paciente.querySelector('.info-HTML')
+var HTML = parseInt(valorHTML.textContent)
+console.log(HTML)
+
+var valorCSc = paciente.querySelector('.info-CSc')
+var CSc = parseInt(valorCSc.textContent)
+console.log(CSc)
+
+var valorJavaScript = paciente.querySelector('.info-JavaScript')
+var JavaScript = parseInt(valorJavaScript.textContent)
+console.log(JavaScript)
+
+var valorMedia = (HTML + CSc + JavaScript) / 3
+console.log(valorMedia)
+
+var substituirMedia = paciente.querySelector('.info-Media')
+substituirMedia.textContent = valorMedia.toFixed(2)
+
+////
+
+
+
+
+
